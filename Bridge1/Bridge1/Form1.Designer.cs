4336dda5ad10855694311c8e7d458175d25961bb
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-05-21 14:56:45 +0700

    init

---
+++ Bridge1/Bridge1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Bridge1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbColour = new System.Windows.Forms.ComboBox();
+            this.cbClothes = new System.Windows.Forms.ComboBox();
+            this.startb = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labFabric = new System.Windows.Forms.Label();
+            this.labB = new System.Windows.Forms.Label();
+            this.labR = new System.Windows.Forms.Label();
+            this.labY = new System.Windows.Forms.Label();
+            this.labK = new System.Windows.Forms.Label();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.restartb = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbColour
+            // 
+            this.cbColour.FormattingEnabled = true;
+            this.cbColour.Items.AddRange(new object[] {
+            "Белый",
+            "Черный",
+            "Красный",
+            "Зеленый"});
+            this.cbColour.Location = new System.Drawing.Point(66, 76);
+            this.cbColour.Name = "cbColour";
+            this.cbColour.Size = new System.Drawing.Size(121, 24);
+            this.cbColour.TabIndex = 0;
+            this.cbColour.Text = "Белый";
+            // 
+            // cbClothes
+            // 
+            this.cbClothes.FormattingEnabled = true;
+            this.cbClothes.Items.AddRange(new object[] {
+            "Футболка",
+            "Брюки",
+            "Платье"});
+            this.cbClothes.Location = new System.Drawing.Point(66, 123);
+            this.cbClothes.Name = "cbClothes";
+            this.cbClothes.Size = new System.Drawing.Size(121, 24);
+            this.cbClothes.TabIndex = 1;
+            this.cbClothes.Text = "Футболка";
+            // 
+            // startb
+            // 
+            this.startb.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.startb.Location = new System.Drawing.Point(43, 214);
+            this.startb.Name = "startb";
+            this.startb.Size = new System.Drawing.Size(130, 61);
+            this.startb.TabIndex = 2;
+            this.startb.Text = "В производство";
+            this.startb.UseVisualStyleBackColor = false;
+            this.startb.Click += new System.EventHandler(this.startb_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Цвет";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вещь";
+            // 
+            // labFabric
+            // 
+            this.labFabric.AutoSize = true;
+            this.labFabric.Location = new System.Drawing.Point(230, 57);
+            this.labFabric.Name = "labFabric";
+            this.labFabric.Size = new System.Drawing.Size(128, 17);
+            this.labFabric.TabIndex = 5;
+            this.labFabric.Text = "Ткань (кв.м): 1000";
+            // 
+            // labB
+            // 
+            this.labB.AutoSize = true;
+            this.labB.Location = new System.Drawing.Point(230, 100);
+            this.labB.Name = "labB";
+            this.labB.Size = new System.Drawing.Size(172, 17);
+            this.labB.TabIndex = 6;
+            this.labB.Text = "Синий краситель (г): 100";
+            // 
+            // labR
+            // 
+            this.labR.AutoSize = true;
+            this.labR.Location = new System.Drawing.Point(230, 78);
+            this.labR.Name = "labR";
+            this.labR.Size = new System.Drawing.Size(189, 17);
+            this.labR.TabIndex = 7;
+            this.labR.Text = "Красный краситель (г): 100";
+            // 
+            // labY
+            // 
+            this.labY.AutoSize = true;
+            this.labY.Location = new System.Drawing.Point(230, 121);
+            this.labY.Name = "labY";
+            this.labY.Size = new System.Drawing.Size(185, 17);
+            this.labY.TabIndex = 8;
+            this.labY.Text = "Желтый краситель (г): 100";
+            // 
+            // labK
+            // 
+            this.labK.AutoSize = true;
+            this.labK.Location = new System.Drawing.Point(230, 143);
+            this.labK.Name = "labK";
+            this.labK.Size = new System.Drawing.Size(183, 17);
+            this.labK.TabIndex = 9;
+            this.labK.Text = "Черный краситель (г): 100";
+            // 
+            // nudAmount
+            // 
+            this.nudAmount.Location = new System.Drawing.Point(109, 168);
+            this.nudAmount.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(78, 22);
+            this.nudAmount.TabIndex = 10;
+            this.nudAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Количество";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Bridge1.Properties.Resources.file_20190607_52780_sqgpem;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(-17, -48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(709, 519);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(233, 167);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(427, 114);
+            this.logBox.TabIndex = 13;
+            // 
+            // restartb
+            // 
+            this.restartb.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.restartb.Location = new System.Drawing.Point(461, 79);
+            this.restartb.Name = "restartb";
+            this.restartb.Size = new System.Drawing.Size(130, 61);
+            this.restartb.TabIndex = 14;
+            this.restartb.Text = "Обновить сырье";
+            this.restartb.UseVisualStyleBackColor = false;
+            this.restartb.Click += new System.EventHandler(this.restartb_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(229, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 24);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Швейное производство";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 296);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.restartb);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudAmount);
+            this.Controls.Add(this.labK);
+            this.Controls.Add(this.labY);
+            this.Controls.Add(this.labR);
+            this.Controls.Add(this.labB);
+            this.Controls.Add(this.labFabric);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startb);
+            this.Controls.Add(this.cbClothes);
+            this.Controls.Add(this.cbColour);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbColour;
+        private System.Windows.Forms.ComboBox cbClothes;
+        private System.Windows.Forms.Button startb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labFabric;
+        private System.Windows.Forms.Label labB;
+        private System.Windows.Forms.Label labR;
+        private System.Windows.Forms.Label labY;
+        private System.Windows.Forms.Label labK;
+        private System.Windows.Forms.NumericUpDown nudAmount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.Button restartb;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Bridge1/Bridge1/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bridge1
+{   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        float fabric = 1000;
+        float Rdye = 100;
+        float Bdye = 100;
+        float Ydye = 100;
+        float Kdye = 100;
+
+        private void startb_Click(object sender, EventArgs e)
+        {
+            int Amount = (int)nudAmount.Value;
+            Colour colour = new ColourWhite();
+            Clothes clothes;
+            switch (cbColour.SelectedIndex)
+            {               
+                case 1:
+                    {
+                        colour = new ColourBlack();
+                        break;
+                    }
+                case 2:
+                    {
+                        colour = new ColourRed();
+                        break;
+                    }
+                case 3:
+                    {
+                        colour = new ColourGreen();
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            switch (cbClothes.SelectedIndex)
+            {
+                default:
+                    {
+                        clothes = new TShirt(colour);
+                        break;
+                    }
+                case 1:
+                    {
+                        clothes = new Pants(colour);
+                        break;
+                    }
+                case 2:
+                    {
+                        clothes = new Dress(colour);
+                        break;
+                    }
+
+            }
+
+            clothes.Produce(ref fabric, ref Rdye,ref Bdye,ref Ydye,ref Kdye,Amount);
+
+            labFabric.Text = "Ткань (кв.м): " + fabric;
+            labR.Text = "Красный краситель (г): " + Rdye;
+            labB.Text = "Синий краситель (г): " + Bdye;
+            labY.Text = "Желтый краситель (г): " + Ydye;
+            labK.Text = "Черный краситель (г): " + Kdye;
+
+
+            logBox.Text = logBox.Text + clothes.Log(Amount);
+
+        }
+
+        private void restartb_Click(object sender, EventArgs e)
+        {
+            if (fabric != 1000 || Rdye != 100 || Bdye != 100 || Ydye != 100 || Kdye != 100)
+            {
+                fabric = 1000;
+                Rdye = 100;
+                Bdye = 100;
+                Ydye = 100;
+                Kdye = 100;
+                labFabric.Text = "Ткань (кв.м): " + fabric;
+                labR.Text = "Красный краситель (г): " + Rdye;
+                labB.Text = "Синий краситель (г): " + Bdye;
+                labY.Text = "Желтый краситель (г): " + Ydye;
+                labK.Text = "Черный краситель (г): " + Kdye;
+                logBox.Text = logBox.Text + DateTime.Now.ToString() + " Обновление сырья" + Environment.NewLine;
+            }
+            else MessageBox.Show("Склад заполнен!");
+        }
+    }
+
+    class Clothes
+    {
+        protected Colour _imp;
+        
+        public Clothes(Colour colour)
+        {
+            this._imp = colour;
+        }
+
+        public virtual void Produce(ref float fabric, ref float r, ref float b, ref float y, ref float k, int a)
+        {
+
+        }
+
+        public virtual string Log(int a)
+        {
+            string text="";
+            return text;
+        }
+    }
+
+    class TShirt : Clothes
+    {
+        public TShirt(Colour colour):base(colour)
+        {
+            
+        }
+
+        public override void Produce(ref float fabric, ref float r, ref float b, ref float y, ref float k, int a)
+        {
+            if (fabric < 2 * a) MessageBox.Show("Недостаточно ткани");
+            else
+            {
+                float i = (float)(a * 0.75);
+                base._imp.Paint(ref r, ref b, ref y, ref k, ref i);
+                if (i != 0) fabric = (float)(fabric - (2 * a));
+            }
+        }
+
+        public override string Log(int a)
+        {
+            string text = DateTime.Now.ToString() +" Произведено " + base._imp.ColourText() + " футболки " + a + " шт."+ Environment.NewLine;
+            return text;
+        }
+
+
+    }
+
+    class Pants : Clothes
+    {
+        public Pants(Colour colour) : base(colour)
+        {
+
+        }
+
+        public override void Produce(ref float fabric, ref float r, ref float b, ref float y, ref float k, int a)
+        {
+            if (fabric < 4 * a) MessageBox.Show("Недостаточно ткани");
+            else
+            {
+                float i = (float)(a * 1.5);
+                base._imp.Paint(ref r, ref b, ref y, ref k, ref i);
+                if (i != 0) fabric = (float)(fabric - (4 * a));
+            }
+        }
+
+        public override string Log(int a)
+        {
+            string text = DateTime.Now.ToString() + " Произведено " + base._imp.ColourText() + " брюк " + a + " шт." + Environment.NewLine;
+            return text;
+        }
+
+
+    }
+
+    class Dress : Clothes
+    {
+        public Dress(Colour colour) : base(colour)
+        {
+
+        }
+
+        public override void Produce(ref float fabric, ref float r, ref float b, ref float y, ref float k, int a)
+        {
+            if (fabric < 3.5 * a) MessageBox.Show("Недостаточно ткани");
+            else
+            {                
+                float i = (float)(a * 1.25);
+                base._imp.Paint(ref r, ref b, ref y, ref k, ref i);
+                if (i != 0) fabric = (float)(fabric - (3.5 * a));
+            }
+        }
+
+        public override string Log(int a)
+        {
+            string text = DateTime.Now.ToString() + " Произведено " + base._imp.ColourText() + " платьев " + a + " шт." + Environment.NewLine;
+            return text;
+        }
+    }
+
+    interface Colour
+    {
+        void Paint(ref float r, ref float b, ref float y, ref float k, ref float i);
+        string ColourText();
+    }
+
+    class ColourWhite : Colour
+    {
+        public void Paint(ref float r, ref float b, ref float y, ref float k, ref float i)
+        {
+            
+        }
+
+        public string ColourText()
+        {
+            string text = "бел.";
+            return text;
+        }
+    }
+
+    class ColourBlack : Colour
+    {
+        public void Paint(ref float r, ref float b, ref float y, ref float k, ref float i)
+        {
+            if (k < i)
+            {
+                i = 0;
+                MessageBox.Show("Недостаточно черного красителя");
+            }
+            else k = k - i;
+        }
+
+        public string ColourText()
+        {
+            string text = "черн.";
+            return text;
+        }
+    }
+
+    class ColourRed : Colour
+    {
+        public void Paint(ref float r, ref float b, ref float y, ref float k, ref float i)
+        {
+            if (r < i)
+            {
+                i = 0;
+                MessageBox.Show("Недостаточно красного красителя");
+            }
+            else r = r - i;
+        }
+
+        public string ColourText()
+        {
+            string text = "красн.";
+            return text;
+        }
+    }
+
+    class ColourGreen : Colour
+    {
+        public void Paint(ref float r, ref float b, ref float y, ref float k, ref float i)
+        {
+            if (b < i / 2 || y < i / 2)
+            {
+                i = 0;
+                MessageBox.Show("Недостаточно красителей");
+            }
+            else
+            {
+                b = b - i / 2;
+                y = y - i / 2;
+            }
+        }
+
+        public string ColourText()
+        {
+            string text = "зел.";
+            return text;
+        }
+    }
+}
